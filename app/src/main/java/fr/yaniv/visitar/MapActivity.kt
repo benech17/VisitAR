1a1fc8acbc26722cee377a5e05a3e8f91046fc06
MightyPotato9 <colinprudhomme23@gmail.com>
2022-06-01 00:33:34 +0200

    Revert "with drawables"
    This reverts commit 6ce0099328fb1bb19e1871d3cea0493d38531337.

--- app/src/main/java/fr/yaniv/visitar/MapActivity.kt
+++ app/src/main/java/fr/yaniv/visitar/MapActivity.kt
@@ -1,18 +1,53 @@
 package fr.yaniv.visitar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.layers.generated.lineLayer
+import com.mapbox.maps.extension.style.layers.properties.generated.LineCap
+import com.mapbox.maps.extension.style.layers.properties.generated.LineJoin
+import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
+import com.mapbox.maps.extension.style.style
+
 
 var mapView: MapView? = null
 
 class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
-        mapView = findViewById(R.id.mapView)
-        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+
+        val GEOJSON_SOURCE_ID = "LineString"
+        val resources = this.getResources()
+        val polygonFeatureJson = resources.openRawResource(R.raw.carte_projet_pmr).bufferedReader().use{ it.readText() }
+        val itineraire = Feature.fromJson(polygonFeatureJson)
+
+        val mapView = MapView(this)
+        setContentView(mapView)
+        // mapView = findViewById(R.id.mapView)
+        mapView.getMapboxMap().setCamera(
+            CameraOptions.Builder().center(
+                Point.fromLngLat(
+                    2.344173, 48.856017
+                )
+            ).zoom(14.0).build()
+        )
+        mapView.getMapboxMap().loadStyle(style(styleUri = Style.MAPBOX_STREETS) {
+            +geoJsonSource(GEOJSON_SOURCE_ID) {
+                feature(itineraire)
+            }
+            +lineLayer("linelayer", GEOJSON_SOURCE_ID) {
+                lineCap(LineCap.ROUND)
+                lineJoin(LineJoin.ROUND)
+                lineOpacity(0.7)
+                lineWidth(8.0)
+                lineColor("red")
+            }
+        }
+        )
     }
 
     override fun onStart() {

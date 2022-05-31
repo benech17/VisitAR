c7b5a60afad8e3415b1f8a0dfae0d1d950e6c5f3
MightyPotato9 <colinprudhomme23@gmail.com>
2022-05-31 19:09:26 +0200

    map

--- app/src/main/java/fr/yaniv/visitar/MainActivity.kt
+++ app/src/main/java/fr/yaniv/visitar/MainActivity.kt
@@ -1,11 +1,19 @@
 package fr.yaniv.visitar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapButton = findViewById<Button>(R.id.buttonMap)
+        mapButton.setOnClickListener {
+            val switchActivityIntent = Intent(this, MapActivity::class.java)
+            startActivity(switchActivityIntent)
+        }
     }
 }

ec8f4d616537512bdf880bc4cee7ab8405dee8fd
MightyPotato9 <colinprudhomme23@gmail.com>
2022-06-01 18:47:46 +0200

    circle layer

--- app/src/main/java/fr/yaniv/visitar/MapActivity.kt
+++ app/src/main/java/fr/yaniv/visitar/MapActivity.kt
@@ -1,13 +1,20 @@
 package fr.yaniv.visitar
 
+import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.geojson.Feature
+import com.mapbox.geojson.FeatureCollection
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.image.image
+import com.mapbox.maps.extension.style.layers.generated.circleLayer
 import com.mapbox.maps.extension.style.layers.generated.lineLayer
+import com.mapbox.maps.extension.style.layers.generated.symbolLayer
+import com.mapbox.maps.extension.style.layers.properties.generated.IconAnchor
 import com.mapbox.maps.extension.style.layers.properties.generated.LineCap
 import com.mapbox.maps.extension.style.layers.properties.generated.LineJoin
 import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
@@ -20,33 +27,51 @@ class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val GEOJSON_SOURCE_ID = "LineString"
+        val LINE_SOURCE_ID = "LineString"
+        val POINT_SOURCE_ID = "PointString"
+        val RED_ICON_ID = "PointIcon"
         val resources = this.getResources()
         val polygonFeatureJson = resources.openRawResource(R.raw.carte_projet_pmr).bufferedReader().use{ it.readText() }
-        val itineraire = Feature.fromJson(polygonFeatureJson)
+        val data = FeatureCollection.fromJson(polygonFeatureJson)
+        val line = data.features()!!.get(0)
+        data.features()!!.removeAt(0)
 
-        val mapView = MapView(this)
-        setContentView(mapView)
-        // mapView = findViewById(R.id.mapView)
-        mapView.getMapboxMap().setCamera(
+        //val mapView = MapView(this)
+        setContentView(R.layout.activity_map)
+        mapView = findViewById(R.id.mapView)
+        mapView!!.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
+        mapView!!.getMapboxMap().setCamera(
             CameraOptions.Builder().center(
                 Point.fromLngLat(
                     2.344173, 48.856017
                 )
             ).zoom(14.0).build()
         )
-        mapView.getMapboxMap().loadStyle(style(styleUri = Style.MAPBOX_STREETS) {
-            +geoJsonSource(GEOJSON_SOURCE_ID) {
-                feature(itineraire)
+        mapView!!.getMapboxMap().loadStyle(
+            styleExtension = style(Style.MAPBOX_STREETS) {
+                /*
+                +image(RED_ICON_ID) {
+                    bitmap(BitmapFactory.decodeResource(resources, R.drawable.ic_red_marker))
+                }
+                */
+                +geoJsonSource(LINE_SOURCE_ID) {
+                    feature(line)
+                }
+                +geoJsonSource(POINT_SOURCE_ID) {
+                    featureCollection(data)
+                }
+                +lineLayer("linelayer", LINE_SOURCE_ID) {
+                    lineCap(LineCap.ROUND)
+                    lineJoin(LineJoin.ROUND)
+                    lineOpacity(0.7)
+                    lineWidth(8.0)
+                    lineColor("red")
+                }
+
+                +circleLayer("pointsLayer", POINT_SOURCE_ID) {
+
+                }
             }
-            +lineLayer("linelayer", GEOJSON_SOURCE_ID) {
-                lineCap(LineCap.ROUND)
-                lineJoin(LineJoin.ROUND)
-                lineOpacity(0.7)
-                lineWidth(8.0)
-                lineColor("red")
-            }
-        }
         )
     }
 
